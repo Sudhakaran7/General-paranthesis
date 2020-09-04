72ad63d39861018727ee97c4e451f8ad3eb54f3a
Sudhakaran_roni <32321500+Sudhakaran7@users.noreply.github.com>
2020-09-04 12:23:17 +0700

    Create generate.py

---
+++ generate.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    # @param an integer
+    # @return a list of string
+    def generateParenthesis(self, n):
+        result = []
+        self.generateParenthesisRecu(result, "", n, n)
+        return result
+
+    def generateParenthesisRecu(self, result, current, left, right):
+        if left == 0 and right == 0:
+            result.append(current)
+        if left > 0:
+            self.generateParenthesisRecu(result, current + "(", left - 1, right)
+        if left < right:
+            self.generateParenthesisRecu(result, current + ")", left, right - 1)
+val=Solution()
+n=int(input())
+print(*val.generateParenthesis(n))
